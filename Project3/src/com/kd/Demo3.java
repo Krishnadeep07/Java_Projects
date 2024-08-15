9f16d56c6022c934f907968b6b00d4e746eb1476
Krishnadeep07 <krishnadeeparumilli@gmail.com>
2024-08-14 23:50:34 -0400

    Initial commit

---
+++ Project3/src/com/kd/Demo3.java
@@ -0,0 +1,16 @@
+package com.kd;
+
+public class Demo3 {
+
+	public static int multiply(int a, int b) {
+		int mul= a * b;
+		return mul;
+	}
+	public static void main(String[] args) {
+		int a=5, b=4;
+		int result= multiply(a,b);
+		System.out.println("The result is "+ result);
+
+	}
+
+}
