78540ad779643cdd2f21b8ea3550941e3a4a2b62
Krishnadeep07 <krishnadeeparumilli@gmail.com>
2024-08-14 23:15:44 -0400

    Initial commit

---
+++ Project2/src/com/kd/Demo2.java
@@ -0,0 +1,12 @@
+package com.kd;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		int a=5, b=4, sum;
+		sum= a+b;
+		System.out.println("The sum of two numbers is " + sum);
+
+	}
+
+}
